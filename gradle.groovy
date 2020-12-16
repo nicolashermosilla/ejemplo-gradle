4740fa3d8d10f2926bd4e58931be0c592ce9e29c
nicolashermosilla <nhermosilla1987@gmail.com>
2020-12-15 21:48:38 -0300

    add groovy

--- gradle.groovy
+++ gradle.groovy
@@ -10,8 +10,6 @@ def call(){
             sh "./gradlew clean build"
         }
         stage("Sonar") {
-            
-            
                 def scannerHome = tool 'sonar scanner';
                 withSonarQubeEnv('sonar') { // If you have configured more than one global server connection, you can specify its name
                 sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
@@ -25,11 +23,7 @@ def call(){
             sh "curl -X GET localhost:8085/rest/mscovid/test?msg=testing"
         }
         stage("Nexus") {
-            
-
             nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/nicolas/code/estudios/usach/unidad3/ejemplo-gradle/build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
-
-
         }
 
 }
