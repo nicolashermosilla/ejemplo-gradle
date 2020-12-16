66141814b88872c4b1a1a5886b17915ad0212e71
nicolashermosilla <nhermosilla1987@gmail.com>
2020-12-15 22:27:02 -0300

    fix

--- maven.groovy
+++ maven.groovy
@@ -5,45 +5,28 @@
 */
 
 def call(){
-  
-
-   
-           
             stage("Build") {
-                steps {
-                    
                     sh "./mvnw clean compile -e"
-                }
             }
             stage("Test") {
-                steps {
-                   
                     sh "./mvnw clean test -e"
-                }
+            
             }
             stage("Jar") {
-                steps {
-                    
-                    sh "./mvnw clean package -e"
-                }
+            sh "./mvnw clean package -e"
+                
             }
 
             stage('SonarQube') {
-                 steps {
                      withSonarQubeEnv(installationName: 'sonar') {
                      sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
-                 }
+                 
                  }
              }
             stage("Upload Nexus") {
-                steps {
-                    
                     nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/nicolas/code/estudios/usach/unidad3/forks/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
 
-                    }
             }
-  
-
 }
 
 return this;
